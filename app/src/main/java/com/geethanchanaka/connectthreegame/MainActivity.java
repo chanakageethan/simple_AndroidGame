f6d3f1e80f3355313cd94d3587f0572746c5c826
geethanchanaka <chanakageethan@gmail.com>
2020-06-22 22:19:32 +0530

    First commit

---
+++ app/src/main/java/com/geethanchanaka/connectthreegame/MainActivity.java
@@ -0,0 +1,42 @@
+package com.geethanchanaka.connectthreegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 0 = Yello , 1 = red
+    int activePlayer = 0;
+
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+         if(activePlayer == 0){
+
+             counter.setImageResource(R.drawable.yellow);
+             activePlayer = 1;
+         }else{
+             counter.setImageResource(R.drawable.red);
+             activePlayer = 0;
+         }
+
+
+        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
