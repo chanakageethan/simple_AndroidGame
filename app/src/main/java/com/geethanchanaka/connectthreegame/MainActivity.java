740d779a6abd5b2870e64935a2c2a1cf648cde3d
geethanchanaka <chanakageethan@gmail.com>
2020-06-23 23:54:25 +0530

    add splash activity

--- app/src/main/java/com/geethanchanaka/connectthreegame/MainActivity.java
+++ app/src/main/java/com/geethanchanaka/connectthreegame/MainActivity.java
@@ -1,37 +1,125 @@
 package com.geethanchanaka.connectthreegame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // 0 = Yello , 1 = red
     int activePlayer = 0;
+    Boolean gameIsActive = true;
 
     //2 means unplayed
-    int [] gameState = {2,2,2,2,2,2,2,2,2};
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPossitions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
 
     public void dropIn(View view) {
 
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(-1000f);
+        System.out.println(counter.getTag().toString());
 
-         if(activePlayer == 0){
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-             counter.setImageResource(R.drawable.yellow);
-             activePlayer = 1;
-         }else{
-             counter.setImageResource(R.drawable.red);
-             activePlayer = 0;
-         }
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
+
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
 
 
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+            for (int[] winningPossition : winningPossitions) {
+                if (gameState[winningPossition[0]] == gameState[winningPossition[1]] &&
+                        gameState[winningPossition[1]] == gameState[winningPossition[2]] &&
+                        gameState[winningPossition[0]] != 2) {
+
+                    String winner = "Red";
+                    if (gameState[winningPossition[0]] == 0) {
+                        winner = "Yello";
+                    }
+
+                    //Someone has won!
+                    gameIsActive = false;
+
+                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+
+                    winnerMessage.setText(winner + " Has Won!");
+
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+
+                    layout.setVisibility(View.VISIBLE);
+                } else {
+
+                    boolean gameIsOver = true;
+                    for (int counterState : gameState) {
+                        if (counterState == 2)  gameIsOver = false;
+
+                    }
+
+                    if (gameIsOver){
+                        TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+
+                        winnerMessage.setText( " It's a draw");
+
+                        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+
+                        layout.setVisibility(View.VISIBLE);
+
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+    public void playAgain(View view) {
+
+
+        gameIsActive = true;
+        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+
+
+        // 0 = Yello , 1 = red
+        activePlayer = 0;
+
+
+        //2 means unplayed
+
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
 
 
     }
